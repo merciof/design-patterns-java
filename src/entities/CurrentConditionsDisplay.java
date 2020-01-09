3db601b09784e85d16bbea83de359a1af0d0c929
merciof <mercio.andrade@ufrpe.br>
2020-01-09 11:49:58 -0300

    CurrentConditionDisplay class added

---
+++ src/entities/CurrentConditionsDisplay.java
@@ -0,0 +1,29 @@
+package entities;
+
+import interfaces.DisplayElement;
+import interfaces.Observer;
+import interfaces.Subject;
+
+public class CurrentConditionsDisplay implements Observer, DisplayElement {
+
+	private float temperature;
+	private float humidity;
+	private Subject weatherData;
+	
+	public CurrentConditionsDisplay(Subject weatherData) {
+		this.weatherData = weatherData;
+		weatherData.registerObserver(this);
+	}
+	
+	@Override
+	public void display() {
+		System.out.println("Current Conditions: " + temperature + "F degrees and "
+							+ humidity + "% humidity");
+	}
+
+	@Override
+	public void update(float temperature, float humidity, float pressure) {
+		this.temperature = temperature;
+		this.humidity = humidity;
+	}
+}
